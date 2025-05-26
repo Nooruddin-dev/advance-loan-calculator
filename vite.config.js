94064cb1a3fb0aca220b58d24bab011f1a1b37df
Nooruddin-dev <nooruddin.9800@gmail.com>
2025-05-26 15:46:53 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/advance-loan-calculator/', // 👈 set to your repo name
   plugins: [react()],
 })
